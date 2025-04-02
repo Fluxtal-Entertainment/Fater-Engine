e298ea629d38a2e4d6aa553076e45777da748dd8
Bigblazej <129090654+bigblazej@users.noreply.github.com>
2025-04-02 17:57:49 +0200

    update

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -1,5 +1,6 @@
 #include "logger.h"
 #include "asserts.h"
+#include "platform/platform.h"
 
 //TODO: temporary
 #include <stdio.h>
@@ -20,11 +21,12 @@ void shutdown_logging()
 void log_output(log_level level, const char* message, ...)
 {
     const char* level_strings[6] = {"[FATAL]: ",  "[ERROR]: ", "[WARN]: ", "[INFO]: ", "[DEBUG]: ", "[TRACE]: "};
-    //b8 is_error = level < 2;
+    b8 is_error = level < LOG_LEVEL_WARN;
 
     //Technically imposes a 32k character limit on a single log entry, but...
     //DON'T DO THAT
-    char out_message[32000];
+    const i32 msg_length = 32000;
+    char out_message[msg_length];
     memset(out_message, 0, sizeof(out_message));
 
     //Format original message. NOTE: Oddly enough, MS's headers override the GCC/Clang va_list type with a "typedef char* va_list" in some cases and a result throws a strange error here. The workaround for now is to use __builtin_va_list, whitch is the type GCC/Clang's va_start expects.
@@ -36,8 +38,15 @@ void log_output(log_level level, const char* message, ...)
     char out_message2[32000];
     sprintf(out_message2, "%s%s\n", level_strings[level], out_message);
 
-    //TODO: Platform-specific output.
-    printf("%s", out_message2);
+    //Platform-specific output.
+    if(is_error)
+    {
+        platform_console_write_error(out_message2, level);
+    }
+    else
+    {
+        platform_console_write(out_message2, level);
+    }
 }
 
 void report_assertion_faliure(const char* expression, const char* message, const char* file, i32 line)
--- engine/src/platform/platform_win32.c
+++ engine/src/platform/platform_win32.c
@@ -6,6 +6,7 @@
 #include "core/logger.h"
 #include <windows.h>
 #include <windowsx.h> //param input extraction
+#include <stdlib.h>
 
 typedef struct internal_state
 {
@@ -13,23 +14,27 @@ typedef struct internal_state
     HWND hwnd;
 } internal_state;
 
+//Clock
+static f64 clock_frequency;
+static LARGE_INTEGER start_time;
+
 LRESULT CALLBACK win32_process_message(HWND hwnd, u32 msg, WPARAM w_param, LPARAM l_param);
 
 b8 platform_startup(platform_state *plat_state, const char *application_name, i32 x, i32 y, i32 width, i32 height)
 {
-    plat_state->internal_state = malloc(sizeof(internal_state));
-    internal_state *state = (internal_state *)plat_state->internal_state;
+    plat_state -> internal_state = malloc(sizeof(internal_state));
+    internal_state *state = (internal_state *)plat_state -> internal_state;
     state->h_instance  = GetModuleHandleA(0);
 
     //Setup and register window class
-    HICON icon = LoadIcon(state->h_instance, IDI_APPLICATION);
+    HICON icon = LoadIcon(state -> h_instance, IDI_APPLICATION);
     WNDCLASSA wc;
     memset(&wc, 0, sizeof(wc));
     wc.style = CS_DBLCLKS; //Get double-clicks
     wc.lpfnWndProc = win32_process_message;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
-    wc.hInstance = state->h_instance;
+    wc.hInstance = state -> h_instance;
     wc.hIcon = icon;
     wc.hCursor = LoadCursor(NULL, IDC_ARROW); //NULL;  Manage the cursor manually
     wc.hbrBackground = NULL;    //transparent
@@ -87,14 +92,165 @@ b8 platform_startup(platform_state *plat_state, const char *application_name, i3
     //If start maximized use "SW_SHOWMAXIMIZED : SW_MAXIMIZE" , if start minimized use "SW_MINIMIZE : SW_SHOWMINNOACTIVE"
     ShowWindow(state -> hwnd, show_window_command_flags);
 
-
+    //Clock setup
+    LARGE_INTEGER frequency;
+    QueryPerformanceFrequency(&frequency);
+    clock_frequency = 1.0 / (f64)frequency.QuadPart;
+    QueryPerformanceCounter(&start_time);
 
     return TRUE;
 }
 
 void platform_shutdown(platform_state *plat_state)
 {
-    
+    //Cold-cast to known type
+    internal_state *state = (internal_state *)plat_state -> internal_state;
+
+    if(state -> hwnd)
+    {
+        DestroyWindow(state -> hwnd);
+        state -> hwnd = 0;
+    }    
 }
 
-#endif
+b8 platform_pump_messages(platform_state* plat_state)
+{
+    MSG message;
+    while(PeekMessageA(&message, NULL, 0, 0, PM_REMOVE))
+    {
+        TranslateMessage(&message);
+        DispatchMessageA(&message);
+    }
+    return TRUE;
+}
+
+void *platform_allocate(u64 size, b8 aligned)
+{
+    return malloc(size);
+}
+
+void platform_free(void* block, b8 aligned)
+{
+    free(block);
+}
+
+void *platform_zero_memory(void *block, u64 size)
+{
+    return memset(block, 0, size);
+}
+
+void *platform_copy_memory(void *dest, const void *source, u64 size)
+{
+    return memcpy(dest, source, size);
+}
+
+void *platform_set_memory(void *dest, i32 value, u64 size)
+{
+    return memset(dest, value, size);
+}
+
+void platform_console_write(const char* message, u8 color)
+{
+    HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    static u8 levels[6] = {64, 4, 6, 2, 1, 8};    //Fatal, Error, Warn, Info, Debug, Trace
+    SetConsoleTextAttribute(console_handle, levels[color]);
+    OutputDebugStringA(message);
+    u64 length = strlen(message);
+    LPDWORD number_written = 0;
+    WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), message, (DWORD)length, number_written, 0);
+}
+
+void platform_console_write_error(const char* message, u8 color)
+{
+    HANDLE console_handle = GetStdHandle(STD_ERROR_HANDLE);
+    static u8 levels[6] = {64, 4, 6, 2, 1, 8};    //Fatal, Error, Warn, Info, Debug, Trace
+    SetConsoleTextAttribute(console_handle, levels[color]);
+    OutputDebugStringA(message);
+    u64 length = strlen(message);
+    LPDWORD number_written = 0;
+    WriteConsoleA(GetStdHandle(STD_ERROR_HANDLE), message, (DWORD)length, number_written, 0);
+}
+
+f64 platform_get_absolute_time()
+{
+    LARGE_INTEGER now_time;
+    QueryPerformanceCounter(&now_time);
+    return (f64)now_time.QuadPart * clock_frequency;
+}
+
+void platform_sleep(u64 ms)
+{
+    Sleep(ms);
+}
+
+LRESULT CALLBACK win32_process_message(HWND hwnd, u32 msg, WPARAM w_param, LPARAM l_param)
+{
+    switch(msg)
+    {
+        case WM_ERASEBKGND:
+            //Preventing from flickering by letting the OS know that erasing will be handled by application
+            return 1;
+
+        case WM_CLOSE:
+            //TODO: Fire an event to quit the application
+            return 0;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        case WM_SIZE:
+        {
+            //Get updated size
+            /*RECT r;
+            GetClientRect(hwnd, &r);
+            u32 width = r.right - r.left;
+            u32 height = r.bottom - r.top;*/
+
+            //TODO: Fire an event for window resizing
+        } break;
+
+        case WM_KEYDOWN:
+        case WM_SYSKEYDOWN:
+        case WM_KEYUP:
+        case WM_SYSKEYUP:
+        {
+            //Key pressed or released
+            //b8 pressed = (msg == WM_KEYDOWN || msg == WM_SYSKEYDOWN);
+            //TODO: Input processing
+        }break;
+
+        case WM_MOUSEMOVE:
+        {
+            //Mouse move
+           /* i32 x_position = GET_X_LPARAM(l_param);
+            i32 y_position = GET_Y_LPARAM(l_param);*/
+            //TODO: Input processing
+        }break;
+
+        case WM_MOUSEHWHEEL:
+        {
+            /*i32 z_delta = GET_WHEEL_DELTA_WPARAM(wparam);
+            if(z_delta != 0)
+            {
+                //Flatten input to an OS independent (-1,1)
+                z_delta = (z_delta < 0) ? -1 : 1;
+                //TODO: Input processing
+            }*/
+        }break;
+
+        case WM_LBUTTONDOWN:
+        case WM_MBUTTONDOWN:
+        case WM_RBUTTONDOWN:
+        case WM_LBUTTONUP:
+        case WM_MBUTTONUP:
+        case WM_RBUTTONUP:
+        {
+            //b8 pressed = msg == WM_LBUTTONDOWN || msg == WM_RBUTTONDOWN || msg == WM_MBUTTONDOWN;
+            //TODO: Input processing
+        }break;
+    }
+    return DefWindowProcA(hwnd, msg, w_param, l_param);
+}
+
+#endif //KPLARFORM_WINDOWS
