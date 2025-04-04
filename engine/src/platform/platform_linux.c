d6a79de08d53b249e1a93790008d5b6b1d04ea39
Bigblazej <129090654+bigblazej@users.noreply.github.com>
2025-04-04 19:11:41 +0200

    update platform_linux.c

--- engine/src/platform/platform_linux.c
+++ engine/src/platform/platform_linux.c
@@ -1,11 +1,25 @@
 #include "platform.h"
 
+//Linux platform layer
+#if KPLATFORM_LINUX
+
+#include "core/logger.h"
 #include <xcb/xcb.h>
 #include <X11/keysym.h>
 #include <X11/XKBlib.h> //sudo apt-get install libx11-dev OR sudo dnf install libX11-devel (if not installed earlier)
 #include <X11/xlib.h>
 #include <X11/Xlib-xcb.h> //sudo apt-get install libxkbcommon-x11-dev OR sudo dnf install libxkbcommon-x11-devel (if not installed earlier)
 #include <sys/time.h>
+
+#if _POSIX_C_SOURCE >= 199309L
+#include <time.h>   //nanosleep
+#else
+#include <unistd.h>//usleep
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 typedef struct internal_state
 {
@@ -19,7 +33,17 @@ typedef struct internal_state
 
 b8 platform_startup(platform_state* plat_state, const char* application_name, i32 x, i32 y, i32 width, i32 height)
 {
+    //create internal state
+    plat_state -> internal_state = malloc(sizeof(internal_state));
+    internal_state *state = (internal_state *)plat_state -> internal_state;
 
+    //Connect to X server
+    state -> display = XOpenDisplay(NULL);
+    //Turn off key repeats
+    XAutoRepeatOff(state -> display);
+    //Retrive the connection from the display
+    int screen_p = 0;
+    state -> connection = XGetXCBConnection(state -> display);
 }
 
 void plarform_shutdown(platform_state* plat_state)
@@ -76,3 +100,5 @@ void platform_sleep(u64 ms)
 {
 
 }
+
+#endif
