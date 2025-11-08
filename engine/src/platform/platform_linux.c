f4683fe1da4681017fa96da9e9f48291cd639eb1
Blazej Hucz <blazejhucz0@gmail.com>
2025-11-08 15:20:39 +0100

    Refactor error logging in platform_linux.c to use FATAL_LOG for improved clarity; update Vulkan surface creation parameters

--- engine/src/platform/platform_linux.c
+++ engine/src/platform/platform_linux.c
@@ -56,7 +56,7 @@ b8 platform_startup(platform_state* plat_state, const char* application_name, i3
     state -> connection = XGetXCBConnection(state -> display);
     if(xcb_connection_has_error(state -> connection))
     {
-        FATAL("FAILED to connect to X Server via XCB");
+        FATAL_LOG("FAILED to connect to X Server via XCB");
         return FALSE;
     }
     //Get data from X Server
@@ -98,7 +98,7 @@ b8 platform_startup(platform_state* plat_state, const char* application_name, i3
     i32 stream_result = xcb_flush(state -> connection);
     if(stream_result <= 0)
     {
-        FATAL("An ERROR occured when flushing the stream: %d", stream_result);
+        FATAL_LOG("An ERROR occured when flushing the stream: %d", stream_result);
         return FALSE;
     }
     return TRUE;
@@ -277,8 +277,8 @@ b8 platform_create_vulkan_surface(platform_state* plat_state, vulkan_context* co
     internal_state *state = (internal_state *)plat_state->internal_state;
 
     VkXcbSurfaceCreateInfoKHR create_info = {VK_STRUCTURE_TYPE_XCB_SURFACE_CREATE_INFO_KHR};
-    create_info.hinstance = state->h_instance;
-    create_info.hwnd = state->hwnd;
+    create_info.connection = state->connection;
+    create_info.window = state->window;
 
     VkResult result = vkCreateXcbSurfaceKHR(context->instance, &create_info, context->allocator, &state->surface);
     if(result != VK_SUCCESS)
