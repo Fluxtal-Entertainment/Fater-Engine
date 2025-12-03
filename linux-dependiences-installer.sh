a6a9459e87e08c3863efda0f169d370abdcd68d7
Blazej Hucz <blazejhucz0@gmail.com>
2025-12-03 09:20:31 +0100

    Fix package installation commands in linux-dependiences-installer.sh: correct clang installation syntax for apt and dnf package managers

--- linux-dependiences-installer.sh
+++ linux-dependiences-installer.sh
@@ -10,13 +10,13 @@ if command -v pacman ; then
 elif command -v apt ; then
     echo "Installing apt version..."
     sudo apt-get update -y && sudo apt-get install libx11-dev -y && sudo apt-get install libxkbcommon-x11-dev -y
-    sudo apt install vulkan-tools vulkan-validationlayers-dev libvulkan-dev
+    sudo apt install vulkan-tools vulkan-validationlayers-dev libvulkan-dev clang-y
     vkcube
     echo "All Dependiences Installed Successfully."
 elif command -v dnf ; then
     echo "Installing dnf version..."
     sudo dnf update -y && sudo dnf install libX11-devel -y && sudo dnf install libxkbcommon-x11-devel -y
-    sudo dnf install vulkan vulkan-tools vulkan-loader vulkan-validation-layers vulkan-headers
+    sudo dnf install vulkan vulkan-tools vulkan-loader vulkan-validation-layers vulkan-headers clang -y
     vkcube
     echo "All Dependiences Installed Successfully."
 else
