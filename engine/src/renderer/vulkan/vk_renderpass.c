3a096f6e54ebdfa9be0b3d5f05fa258d3b330704
Blazej Hucz <blazejhucz0@gmail.com>
2026-09-09 19:17:47 +0200

    #021 36:40

--- engine/src/renderer/vulkan/vk_renderpass.c
+++ engine/src/renderer/vulkan/vk_renderpass.c
@@ -3,6 +3,10 @@
 
 void vulkan_renderpass_create(vulkan_context* context, vulkan_renderpass* out_renderpass, f32 x, f32 y, f32 w, f32 h, f32 r, f32 g, f32 b, f32 a, f32 depth, u32 stencil)
 {
+    out_renderpass->x = x;
+    out_renderpass->y = y;
+    out_renderpass->w = w;
+    out_renderpass->h = h;
 
     //Main subpass
     VkSubpassDescription subpass = {};
